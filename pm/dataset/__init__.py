42c72d698e76410df172270cf532125184d1ebdd
Nicholas Toohey <ntoohey@gmail.com>
2026-05-21 21:48:02 -0500

    bug fixing

--- pm/dataset/__init__.py
+++ pm/dataset/__init__.py
@@ -1,3 +1,3 @@
-from dataset.dataloader import get_data_loaders, PaintingDataset
+from pm.dataset.dataloader import get_data_loaders, PaintingDataset
 
 __all__ = ['get_data_loaders', 'PaintingDataset']
